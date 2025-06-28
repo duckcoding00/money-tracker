75a83be51af87f41d77141fec61e1515ae0428cc
Ardi Sasongko <ardisasongko002@gmail.com>
2025-06-28 12:44:45 +0700

    init: reinitialization frontend folder

--- money-tracker-frontend/vite.config.ts
+++ money-tracker-frontend/vite.config.ts
@@ -1,5 +1,5 @@
-import tailwindcss from '@tailwindcss/vite';
 import devtoolsJson from 'vite-plugin-devtools-json';
+import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
